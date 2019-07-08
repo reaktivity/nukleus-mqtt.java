c19a31be0fcbb6fc31b187688da4160a2c2c963d
amandaratio <amandaratio@gmail.com>
2019-07-08 13:50:42 -0700

    Controller

--- src/main/java/org/reaktivity/nukleus/mqtt/internal/MqttController.java
+++ src/main/java/org/reaktivity/nukleus/mqtt/internal/MqttController.java
@@ -19,16 +19,36 @@ package org.reaktivity.nukleus.mqtt.internal;
 import static java.nio.ByteBuffer.allocateDirect;
 import static java.nio.ByteOrder.nativeOrder;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.agrona.MutableDirectBuffer;
 import org.agrona.concurrent.UnsafeBuffer;
 import org.reaktivity.nukleus.Controller;
 import org.reaktivity.nukleus.ControllerSpi;
 
+import org.reaktivity.nukleus.route.RouteKind;
+import org.reaktivity.nukleus.mqtt.internal.types.Flyweight;
+import org.reaktivity.nukleus.mqtt.internal.types.OctetsFW;
+import org.reaktivity.nukleus.mqtt.internal.types.control.Role;
+import org.reaktivity.nukleus.mqtt.internal.types.control.RouteFW;
+import org.reaktivity.nukleus.mqtt.internal.types.control.UnrouteFW;
+import org.reaktivity.nukleus.mqtt.internal.types.control.MqttRouteExFW;
+
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public final class MqttController implements Controller
 {
     private static final int MAX_SEND_LENGTH = 1024;
+
+    private final RouteFW.Builder routeRW = new RouteFW.Builder();
+    private final UnrouteFW.Builder unrouteRW = new UnrouteFW.Builder();
+
+    private final MqttRouteExFW.Builder routeExRW = new MqttRouteExFW.Builder();
+
+    private final OctetsFW extensionRO = new OctetsFW().wrap(new UnsafeBuffer(new byte[0]), 0, 0);
 
     private final ControllerSpi controllerSpi;
     private final MutableDirectBuffer commandBuffer;
@@ -66,5 +86,74 @@ public final class MqttController implements Controller
     public String name()
     {
         return MqttNukleus.NAME;
+    }
+
+    public CompletableFuture<Long> route(
+            RouteKind kind,
+            String localAddress,
+            String remoteAddress)
+    {
+        return route(kind, localAddress, remoteAddress, null);
+    }
+
+    public CompletableFuture<Long> route(
+            RouteKind kind,
+            String localAddress,
+            String remoteAddress,
+            String extension)
+    {
+        Flyweight routeEx = extensionRO;
+
+        if (extension != null)
+        {
+            final JsonParser parser = new JsonParser();
+            final JsonElement element = parser.parse(extension);
+            if (element.isJsonObject())
+            {
+                final JsonObject object = (JsonObject) element;
+                final String topic = gson.fromJson(object.get("sensor/one"), String.class);
+
+                routeEx = routeExRW.wrap(extensionBuffer, 0, extensionBuffer.capacity())
+                        .topic(topic)
+                        .build();
+            }
+        }
+
+        return doRoute(kind, localAddress, remoteAddress, routeEx);
+    }
+
+    public CompletableFuture<Void> unroute(
+            long routeId)
+    {
+        final long correlationId = controllerSpi.nextCorrelationId();
+
+        final UnrouteFW unroute = unrouteRW.wrap(commandBuffer, 0, commandBuffer.capacity())
+                .correlationId(correlationId)
+                .nukleus(name())
+                .routeId(routeId)
+                .build();
+
+        return controllerSpi.doUnroute(unroute.typeId(), unroute.buffer(), unroute.offset(), unroute.sizeof());
+    }
+
+    private CompletableFuture<Long> doRoute(
+            RouteKind kind,
+            String localAddress,
+            String remoteAddress,
+            Flyweight extension)
+    {
+        final long correlationId = controllerSpi.nextCorrelationId();
+        final Role role = Role.valueOf(kind.ordinal());
+
+        final RouteFW route = routeRW.wrap(commandBuffer, 0, commandBuffer.capacity())
+                .correlationId(correlationId)
+                .nukleus(name())
+                .role(b -> b.set(role))
+                .localAddress(localAddress)
+                .remoteAddress(remoteAddress)
+                .extension(extension.buffer(), extension.offset(), extension.sizeof())
+                .build();
+
+        return controllerSpi.doRoute(route.typeId(), route.buffer(), route.offset(), route.sizeof());
     }
 }
