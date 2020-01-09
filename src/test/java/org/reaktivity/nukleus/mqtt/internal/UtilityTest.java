935d281d3cd0f54bf25f286b80e8917a326d182d
dknoma <djknoma@gmail.com>
2020-01-09 11:47:40 -0800

    fixed formatting and licences

--- src/test/java/org/reaktivity/nukleus/mqtt/internal/UtilityTest.java
+++ src/test/java/org/reaktivity/nukleus/mqtt/internal/UtilityTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2016-2019 The Reaktivity Project
+ *
+ * The Reaktivity Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package org.reaktivity.nukleus.mqtt.internal;
 
 import static org.junit.Assert.assertFalse;
@@ -9,7 +24,7 @@ import org.junit.Test;
 public class UtilityTest
 {
     @Test
-    public void TestValidTopicFilterRegex()
+    public void testValidTopicFilterRegex()
     {
         final String[] validTopicFilters = new String[]{"#", "/#", "+/+/+/#", "+", "+/", "+/#", "//", "/", "topic/name",
             "/topic/name/", "/topic//name", "topic//name", "/topic/+/name", "/topic/name/#", "topic//+/name/+/#"};
@@ -17,7 +32,7 @@ public class UtilityTest
     }
 
     @Test
-    public void TestInvalidTopicFilterRegex()
+    public void testInvalidTopicFilterRegex()
     {
         final String[] invalidTopicFilters = new String[]{"#/", "##", "/##", "##/", "#+", "++", "++/", "/++", "#/topic/name",
             "#topic", "+topic", "/top+ic/name/", "/topic/#/name", "topic//name+", "/#topic/+/name", "/topic/na#me/#",
@@ -25,7 +40,8 @@ public class UtilityTest
         checkValidityOfTopicFilters(invalidTopicFilters, false);
     }
 
-    private static void checkValidityOfTopicFilters(String[] filters, boolean checkForValid) {
+    private static void checkValidityOfTopicFilters(String[] filters, boolean checkForValid)
+    {
         if (checkForValid)
         {
             for (String s : filters)
